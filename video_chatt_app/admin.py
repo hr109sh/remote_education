caf0d2affbe9bd461a941d0c9d667c8fd7d4e30b
Harshvardhan Kumar <hr109sh@gmail.com>
2020-07-05 19:13:13 +0530

    added models

--- video_chatt_app/admin.py
+++ video_chatt_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from video_chatt_app.models import Role
 
 # Register your models here.
+
+admin.site.register(Role)
+# admin.site.register(MyUserRole)
--- video_chatt_app/models.py
+++ video_chatt_app/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Role(models.Model):
+	name = models.CharField(max_length = 100)
+
+	def __str__(self):
+		return self.name
+
+
+class UserRole(models.Model):
+	user_id= models.ForeignKey(User, on_delete=models.CASCADE)
+	role_id = models.ForeignKey(Role, on_delete=models.CASCADE)
+
+	
+class UserMeeting(models.Model):
+	user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+	meeting_name = models.CharField(max_length = 100)
+	meet_start = models.DateTimeField(auto_now_add=True)
+	meet_end = models.DateTimeField(null=True,blank=True)
+
+
+class User_attendence(models.Model):
+	user_id= models.ForeignKey(User, on_delete=models.CASCADE)
+	meeting_id = models.ForeignKey(UserMeeting,on_delete=models.CASCADE)
+
+class Subject(models.Model):
+	subject_name = models.CharField(max_length = 100)
+
+
+class Topic(models.Model):
+	topic_name = models.CharField(max_length = 100)
+	subject_id = models.ForeignKey(Subject,on_delete=models.CASCADE)
+
+class Questions(models.Model):
+	topic_id = models.ForeignKey(Topic,on_delete = models.CASCADE)
+	question = models.TextField()
+
+class Answer(models.Model):
+	question_id = models.ForeignKey(Questions,on_delete = models.CASCADE)
+	question_answer = models.CharField(max_length = 100)
+
+class subject_teacher(models.Model):
+	subject_id = models.ForeignKey(Subject,on_delete= models.CASCADE)
+	user_id = models.ForeignKey(User,on_delete = models.CASCADE)
+
+class user_parent(models.Model):
+	parent_name = models.CharField(max_length = 100)
+	user_id = models.ForeignKey(User,on_delete = models.CASCADE)
+
+class StudentReport(models.Model):
+	user_id = models.ForeignKey(User,on_delete = models.CASCADE)
+	meeting_id = models.ForeignKey(UserMeeting,on_delete = models.CASCADE)
+	question_id = models.ForeignKey(Questions,on_delete = models.CASCADE)
+	question_time = models.DateTimeField(auto_now_add = True)
+	answer_time = models.DateTimeField(null=True)
+	answer_corrent = models.CharField(max_length = 100)
